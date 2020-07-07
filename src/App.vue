579479099f846225267c93d8cf7c7d8d88436261
Sam McCall <smccall@smccall.com>
2020-07-07 16:54:46 -0400

    Init commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <PokeTweets>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PokeTweets from './components/PokeTweets.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PokeTweets
   }
 }
 </script>
