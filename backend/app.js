175ef569590d0db03cbcd3b01abeaee8dd78177b
Sam McCall <smccall@smccall.com>
2020-07-11 22:36:49 -0400

    Add oauth keys to .gitignore, remove express templating

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// Import routes
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -19,6 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Imported routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
