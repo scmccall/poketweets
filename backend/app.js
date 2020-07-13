cdeef7d85c4cd0a0cdd93dc489cafc3de7d64b9e
Sam McCall <smccall@smccall.com>
2020-07-13 18:02:06 -0400

    Start axios integration

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 // Import routes
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+let tweetsRouter = require('./routes/tweets');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Imported routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/twitter', tweetsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- backend/routes/tweets.js
+++ backend/routes/tweets.js
@@ -1,9 +1,44 @@
 var express = require('express');
 var router = express.Router();
+let axios = require('axios');
+let keys = require("../secretkeys");
 
-/* GET home page. */
+/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let url = "https://api.twitter.com/1.1/statuses/user_timeline.json";
+  let config = {
+    headers: {
+      Authorization: `Bearer ${keys.twitter.bearerToken}`
+    }
+  };
+  let params = {
+    screen_name: "sdaytime",
+  };
+
+  axios.get(url, params, config)
+    .then(res => {
+      console.log(res)
+    })
+    .catch(err => {
+      if (err.response) {
+        console.log(err.response.data);
+        console.log(err.response.status);
+        console.log(err.response.headers);
+      } else if (err.request) {
+        console.log(err.request);
+      } else {
+        console.log(`Error: ${error.message}`);
+      }
+    });
+  
+
+
+  /*
+  request(options).pipe.res;
+  res.send("hello");
+  console.log(`request reached`);
+  */
 });
 
 module.exports = router;
+ 
