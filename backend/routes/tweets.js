fa724718c4133fba4d9fa7c03822c554487e0389
Sam McCall <smccall@smccall.com>
2020-07-14 21:22:57 -0400

    Fix Twitter API functionality

--- backend/routes/tweets.js
+++ backend/routes/tweets.js
@@ -5,23 +5,22 @@ let keys = require("../secretkeys");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  let url = "https://api.twitter.com/1.1/statuses/user_timeline.json";
-
-  let params = {
-    screen_name: "sdaytime",
-  };
+  let screen_name = "sdaytime"
 
   let config = {
+    method: "get",
+    url: `https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=${screen_name}`,
     headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
       Authorization: `Bearer ${keys.twitter.bearerToken}`
     }
   };
 
-  console.log(params)
   console.log(config)
+  console.log(typeof(config.url));
 
   // axios.get(url, config, params)
-    axios.get(url, config, params)
+    axios(config)
     .then(result => {
       res.json(result.data);
       // console.log(res.json(result.data));
