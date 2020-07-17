e5273e405d9f1d464b2b70d5fd9b68bf15857380
Sam McCall <smccall@smccall.com>
2020-07-17 16:14:50 -0400

    Add helper functions to return most significant persoanlity trait
    I created a helper function that takes the Watson API result as
    input and iterates through the 30 personality traits in the Big 5
    categories. It then compares each of the traits and returns the
    trait with the highest percentile score. This trait will be fed to a function that will return the appropriate Pokemon type.

---
+++ backend/watson-helper.js
@@ -0,0 +1,608 @@
+const testData = {
+  "word_count": 15223,
+  "processed_language": "en",
+  "personality": [
+    {
+      "trait_id": "big5_openness",
+      "name": "Openness",
+      "category": "personality",
+      "percentile": 0.8011555009552995,
+      "significant": true,
+      "children": [
+        {
+          "trait_id": "facet_adventurousness",
+          "name": "Adventurousness",
+          "category": "personality",
+          "percentile": 0.8975586904731845,
+          "significant": true
+        },
+        {
+          "trait_id": "facet_artistic_interests",
+          "name": "Artistic interests",
+          "category": "personality",
+          "percentile": 0.9770309419531928,
+          "significant": true
+        },
+        {
+          "trait_id": "facet_emotionality",
+          "name": "Emotionality",
+          "category": "personality",
+          "percentile": 0.9947058875647474,
+          "significant": true
+        },
+        {
+          "trait_id": "facet_imagination",
+          "name": "Imagination",
+          "category": "personality",
+          "percentile": 0.8733065387317427,
+          "significant": true
+        },
+        {
+          "trait_id": "facet_intellect",
+          "name": "Intellect",
+          "category": "personality",
+          "percentile": 0.8717194796402048,
+          "significant": true
+        },
+        {
+          "trait_id": "facet_liberalism",
+          "name": "Authority-challenging",
+          "category": "personality",
+          "percentile": 0.6405414845731188,
+          "significant": true
+        }
+      ]
+    },
+    {
+      "trait_id": "big5_conscientiousness",
+      "name": "Conscientiousness",
+      "category": "personality",
+      "percentile": 0.8100175318417563,
+      "significant": true,
+      "children": [
+        {
+          "trait_id": "facet_achievement_striving",
+          "name": "Achievement striving",
+          "category": "personality",
+          "percentile": 0.8461329922662866,
+          "significant": true
+        },
+        {
+          "trait_id": "facet_cautiousness",
+          "name": "Cautiousness",
+          "category": "personality",
+          "percentile": 0.7220362727004095,
+          "significant": true
+        },
+        {
+          "trait_id": "facet_dutifulness",
+          "name": "Dutifulness",
+          "category": "personality",
+          "percentile": 0.8421638467925526,
+          "significant": true
+        },
+        {
+          "trait_id": "facet_orderliness",
+          "name": "Orderliness",
+          "category": "personality",
+          "percentile": 0.6121858586705439,
+          "significant": true
+        },
+        {
+          "trait_id": "facet_self_discipline",
+          "name": "Self-discipline",
+          "category": "personality",
+          "percentile": 0.8317329416265957,
+          "significant": true
+        },
+        {
+          "trait_id": "facet_self_efficacy",
+          "name": "Self-efficacy",
+          "category": "personality",
+          "percentile": 0.7088313709543814,
+          "significant": true
+        }
+      ]
+    },
+    {
+      "trait_id": "big5_extraversion",
+      "name": "Extraversion",
+      "category": "personality",
+      "percentile": 0.6498079607138149,
+      "significant": true,
+      "children": [
+        {
+          "trait_id": "facet_activity_level",
+          "name": "Activity level",
+          "category": "personality",
+          "percentile": 0.8822058491396555,
+          "significant": true
+        },
+        {
+          "trait_id": "facet_assertiveness",
+          "name": "Assertiveness",
+          "category": "personality",
+          "percentile": 0.668984138017408,
+          "significant": true
+        },
+        {
+          "trait_id": "facet_cheerfulness",
+          "name": "Cheerfulness",
+          "category": "personality",
+          "percentile": 0.9435264775235841,
+          "significant": true
+        },
+        {
+          "trait_id": "facet_excitement_seeking",
+          "name": "Excitement-seeking",
+          "category": "personality",
+          "percentile": 0.5913387477205387,
+          "significant": true
+        },
+        {
+          "trait_id": "facet_friendliness",
+          "name": "Outgoing",
+          "category": "personality",
+          "percentile": 0.9577289025786415,
+          "significant": true
+        },
+        {
+          "trait_id": "facet_gregariousness",
+          "name": "Gregariousness",
+          "category": "personality",
+          "percentile": 0.6494284805198479,
+          "significant": true
+        }
+      ]
+    },
+    {
+      "trait_id": "big5_agreeableness",
+      "name": "Agreeableness",
+      "category": "personality",
+      "percentile": 0.9478612479382098,
+      "significant": true,
+      "children": [
+        {
+          "trait_id": "facet_altruism",
+          "name": "Altruism",
+          "category": "personality",
+          "percentile": 0.9924198382420473,
+          "significant": true
+        },
+        {
+          "trait_id": "facet_cooperation",
+          "name": "Cooperation",
+          "category": "personality",
+          "percentile": 0.8612307420897861,
+          "significant": true
+        },
+        {
+          "trait_id": "facet_modesty",
+          "name": "Modesty",
+          "category": "personality",
+          "percentile": 0.7726811931877458,
+          "significant": true
+        },
+        {
+          "trait_id": "facet_morality",
+          "name": "Uncompromising",
+          "category": "personality",
+          "percentile": 0.8907910233571188,
+          "significant": true
+        },
+        {
+          "trait_id": "facet_sympathy",
+          "name": "Sympathy",
+          "category": "personality",
+          "percentile": 0.994218470874908,
+          "significant": true
+        },
+        {
+          "trait_id": "facet_trust",
+          "name": "Trust",
+          "category": "personality",
+          "percentile": 0.9036111955659848,
+          "significant": true
+        }
+      ]
+    },
+    {
+      "trait_id": "big5_neuroticism",
+      "name": "Emotional range",
+      "category": "personality",
+      "percentile": 0.5496199917809975,
+      "significant": true,
+      "children": [
+        {
+          "trait_id": "facet_anger",
+          "name": "Fiery",
+          "category": "personality",
+          "percentile": 0.1764002205850792,
+          "significant": true
+        },
+        {
+          "trait_id": "facet_anxiety",
+          "name": "Prone to worry",
+          "category": "personality",
+          "percentile": 0.4288307606218654,
+          "significant": true
+        },
+        {
+          "trait_id": "facet_depression",
+          "name": "Melancholy",
+          "category": "personality",
+          "percentile": 0.15019740428715428,
+          "significant": true
+        },
+        {
+          "trait_id": "facet_immoderation",
+          "name": "Immoderation",
+          "category": "personality",
+          "percentile": 0.26916719249301657,
+          "significant": true
+        },
+        {
+          "trait_id": "facet_self_consciousness",
+          "name": "Self-consciousness",
+          "category": "personality",
+          "percentile": 0.3035154334067464,
+          "significant": true
+        },
+        {
+          "trait_id": "facet_vulnerability",
+          "name": "Susceptible to stress",
+          "category": "personality",
+          "percentile": 0.38972068326779474,
+          "significant": true
+        }
+      ]
+    }
+  ],
+  "needs": [
+    {
+      "trait_id": "need_challenge",
+      "name": "Challenge",
+      "category": "needs",
+      "percentile": 0.6736233205451105,
+      "significant": true
+    },
+    {
+      "trait_id": "need_closeness",
+      "name": "Closeness",
+      "category": "needs",
+      "percentile": 0.8380283404181328,
+      "significant": true
+    },
+    {
+      "trait_id": "need_curiosity",
+      "name": "Curiosity",
+      "category": "needs",
+      "percentile": 0.929383931896093,
+      "significant": true
+    },
+    {
+      "trait_id": "need_excitement",
+      "name": "Excitement",
+      "category": "needs",
+      "percentile": 0.7280972568828078,
+      "significant": true
+    },
+    {
+      "trait_id": "need_harmony",
+      "name": "Harmony",
+      "category": "needs",
+      "percentile": 0.9694112904157445,
+      "significant": true
+    },
+    {
+      "trait_id": "need_ideal",
+      "name": "Ideal",
+      "category": "needs",
+      "percentile": 0.6824330657640135,
+      "significant": true
+    },
+    {
+      "trait_id": "need_liberty",
+      "name": "Liberty",
+      "category": "needs",
+      "percentile": 0.7869644002235163,
+      "significant": true
+    },
+    {
+      "trait_id": "need_love",
+      "name": "Love",
+      "category": "needs",
+      "percentile": 0.820799204805859,
+      "significant": true
+    },
+    {
+      "trait_id": "need_practicality",
+      "name": "Practicality",
+      "category": "needs",
+      "percentile": 0.3503620508268486,
+      "significant": true
+    },
+    {
+      "trait_id": "need_self_expression",
+      "name": "Self-expression",
+      "category": "needs",
+      "percentile": 0.8673284357850486,
+      "significant": true
+    },
+    {
+      "trait_id": "need_stability",
+      "name": "Stability",
+      "category": "needs",
+      "percentile": 0.8732565885512342,
+      "significant": true
+    },
+    {
+      "trait_id": "need_structure",
+      "name": "Structure",
+      "category": "needs",
+      "percentile": 0.7456082872690659,
+      "significant": true
+    }
+  ],
+  "values": [
+    {
+      "trait_id": "value_conservation",
+      "name": "Conservation",
+      "category": "values",
+      "percentile": 0.8926822285613933,
+      "significant": true
+    },
+    {
+      "trait_id": "value_openness_to_change",
+      "name": "Openness to change",
+      "category": "values",
+      "percentile": 0.8575991638808613,
+      "significant": true
+    },
+    {
+      "trait_id": "value_hedonism",
+      "name": "Hedonism",
+      "category": "values",
+      "percentile": 0.44128086884055484,
+      "significant": true
+    },
+    {
+      "trait_id": "value_self_enhancement",
+      "name": "Self-enhancement",
+      "category": "values",
+      "percentile": 0.6458578881392517,
+      "significant": true
+    },
+    {
+      "trait_id": "value_self_transcendence",
+      "name": "Self-transcendence",
+      "category": "values",
+      "percentile": 0.8237769534534466,
+      "significant": true
+    }
+  ],
+  "behavior": [
+    {
+      "trait_id": "behavior_sunday",
+      "name": "Sunday",
+      "category": "behavior",
+      "percentage": 0.21392532795156408
+    },
+    {
+      "trait_id": "behavior_monday",
+      "name": "Monday",
+      "category": "behavior",
+      "percentage": 0.425832492431887
+    },
+    {
+      "trait_id": "behavior_tuesday",
+      "name": "Tuesday",
+      "category": "behavior",
+      "percentage": 0.07164480322906155
+    },
+    {
+      "trait_id": "behavior_wednesday",
+      "name": "Wednesday",
+      "category": "behavior",
+      "percentage": 0.011099899091826439
+    },
+    {
+      "trait_id": "behavior_thursday",
+      "name": "Thursday",
+      "category": "behavior",
+      "percentage": 0.12209889001009082
+    },
+    {
+      "trait_id": "behavior_friday",
+      "name": "Friday",
+      "category": "behavior",
+      "percentage": 0.07769929364278506
+    },
+    {
+      "trait_id": "behavior_saturday",
+      "name": "Saturday",
+      "category": "behavior",
+      "percentage": 0.07769929364278506
+    },
+    {
+      "trait_id": "behavior_0000",
+      "name": "0:00 am",
+      "category": "behavior",
+      "percentage": 0.45610494450050454
+    },
+    {
+      "trait_id": "behavior_0100",
+      "name": "1:00 am",
+      "category": "behavior",
+      "percentage": 0.12209889001009082
+    },
+    {
+      "trait_id": "behavior_0200",
+      "name": "2:00 am",
+      "category": "behavior",
+      "percentage": 0.02119071644803229
+    },
+    {
+      "trait_id": "behavior_0300",
+      "name": "3:00 am",
+      "category": "behavior",
+      "percentage": 0.09485368314833502
+    },
+    {
+      "trait_id": "behavior_0400",
+      "name": "4:00 am",
+      "category": "behavior",
+      "percentage": 0.020181634712411706
+    },
+    {
+      "trait_id": "behavior_0500",
+      "name": "5:00 am",
+      "category": "behavior",
+      "percentage": 0
+    },
+    {
+      "trait_id": "behavior_0600",
+      "name": "6:00 am",
+      "category": "behavior",
+      "percentage": 0
+    },
+    {
+      "trait_id": "behavior_0700",
+      "name": "7:00 am",
+      "category": "behavior",
+      "percentage": 0.011099899091826439
+    },
+    {
+      "trait_id": "behavior_0800",
+      "name": "8:00 am",
+      "category": "behavior",
+      "percentage": 0
+    },
+    {
+      "trait_id": "behavior_0900",
+      "name": "9:00 am",
+      "category": "behavior",
+      "percentage": 0
+    },
+    {
+      "trait_id": "behavior_1000",
+      "name": "10:00 am",
+      "category": "behavior",
+      "percentage": 0
+    },
+    {
+      "trait_id": "behavior_1100",
+      "name": "11:00 am",
+      "category": "behavior",
+      "percentage": 0
+    },
+    {
+      "trait_id": "behavior_1200",
+      "name": "12:00 pm",
+      "category": "behavior",
+      "percentage": 0
+    },
+    {
+      "trait_id": "behavior_1300",
+      "name": "1:00 pm",
+      "category": "behavior",
+      "percentage": 0
+    },
+    {
+      "trait_id": "behavior_1400",
+      "name": "2:00 pm",
+      "category": "behavior",
+      "percentage": 0
+    },
+    {
+      "trait_id": "behavior_1500",
+      "name": "3:00 pm",
+      "category": "behavior",
+      "percentage": 0.022199798183652877
+    },
+    {
+      "trait_id": "behavior_1600",
+      "name": "4:00 pm",
+      "category": "behavior",
+      "percentage": 0.022199798183652877
+    },
+    {
+      "trait_id": "behavior_1700",
+      "name": "5:00 pm",
+      "category": "behavior",
+      "percentage": 0.03229061553985873
+    },
+    {
+      "trait_id": "behavior_1800",
+      "name": "6:00 pm",
+      "category": "behavior",
+      "percentage": 0.010090817356205853
+    },
+    {
+      "trait_id": "behavior_1900",
+      "name": "7:00 pm",
+      "category": "behavior",
+      "percentage": 0.011099899091826439
+    },
+    {
+      "trait_id": "behavior_2000",
+      "name": "8:00 pm",
+      "category": "behavior",
+      "percentage": 0.022199798183652877
+    },
+    {
+      "trait_id": "behavior_2100",
+      "name": "9:00 pm",
+      "category": "behavior",
+      "percentage": 0
+    },
+    {
+      "trait_id": "behavior_2200",
+      "name": "10:00 pm",
+      "category": "behavior",
+      "percentage": 0.03128153380423814
+    },
+    {
+      "trait_id": "behavior_2300",
+      "name": "11:00 pm",
+      "category": "behavior",
+      "percentage": 0.1231079717457114
+    }
+  ],
+  "warnings": []
+};
+
+// Returns the trait with the highest percentile score
+findHighestPercentileTrait = (data) => {
+  let big5Categories = data.personality;
+  let highestTrait = null; // Will store the trait with the highest percentile
+
+  // Iterates over all the Big 5 personality categories
+  for (i = 0; i < big5Categories.length; i++) {
+
+    // Iterates over all traits within each Big 5 category
+    for (j = 0; j < big5Categories[i].children.length; j++) {
+      let trait = big5Categories[i].children[j]
+
+      // Calls findGreaterOfTwoTraits() to return the trait with the highest score
+      highestTrait = findGreaterOfTwoTraits(highestTrait, trait);
+    }
+  }
+  console.log(highestTrait);
+  // return highestTrait;
+}
+
+findGreaterOfTwoTraits = (currentTrait, newTrait) => {
+  // Base case for testing the first trait
+  if (currentTrait === null) {
+    return newTrait;
+  } else if (newTrait.percentile > currentTrait.percentile) {
+    return newTrait;
+  } else {
+    return currentTrait;
+  }
+};
+
+
+
+findHighestPercentileTrait(testData);
