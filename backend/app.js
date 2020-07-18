192533413ffa6d2b0b93e5c2b7f4151c3cf873bf
Sam McCall <smccall@smccall.com>
2020-07-18 18:31:57 -0400

    Add Mongoose, initialize database

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
+var mongoose = require('mongoose');
+
+// Set up db
+var mongoDB = 'mongodb://localhost/db';
+mongoose.connect(mongoDB,{ useNewUrlParser: true, useUnifiedTopology: true });
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // Import routes
 var indexRouter = require('./routes/index');
